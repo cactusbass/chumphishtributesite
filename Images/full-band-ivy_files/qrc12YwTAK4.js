45d74e04f9191018aaf6f131b4bc3340bd5339f8
cactusbass <chad.m.weiner@gmail.com>
2025-04-18 09:25:05 -0700

    Update photo caption and add social media links

--- Images/full-band-ivy_files/qrc12YwTAK4.js
+++
@@ -1,4 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("CommerceTabFeedClickFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f,g){"use strict";a=c("getFalcoLogPolicy_DO_NOT_USE")("1878");b=d("FalcoLoggerInternal").create("commerce_tab_feed_click",a);e=b;g["default"]=e}),98);
-__d("CommerceTabFeedImpressionFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f,g){"use strict";a=c("getFalcoLogPolicy_DO_NOT_USE")("2315");b=d("FalcoLoggerInternal").create("commerce_tab_feed_impression",a);e=b;g["default"]=e}),98);
